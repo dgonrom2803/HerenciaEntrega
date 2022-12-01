c8d0bbd0e97dfa416b6c09f8424e019d33395a5f
dgonrom2803 <dgonrom2803@g.educaand.es>
2022-12-01 09:37:12 +0100

    HerenciaEntrega

---
+++ Futbolista.java
@@ -0,0 +1,35 @@
+public class Futbolista {
+    private Integer licencia;
+    private String equipo;
+
+    public Futbolista() {
+    }
+
+    public Futbolista(Integer licencia) {
+        this.licencia = licencia;
+    }
+
+    public Futbolista(String nombre, Integer licencia) {
+        this.licencia = licencia;
+    }
+
+    public String equipo () {
+        return equipo;
+    }
+
+    public Integer getLicencia() {
+        return licencia;
+    }
+
+    public void setLicencia(Integer licencia) {
+        this.licencia = licencia;
+    }
+
+    public String getEquipo() {
+        return equipo;
+    }
+
+    public void setEquipo(String equipo) {
+        this.equipo = equipo;
+    }
+}
